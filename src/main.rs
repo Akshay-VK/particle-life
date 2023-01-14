46ccfe32727aabb06b353c8c0450986b4ff5a4f5
Akshay VK <akshaykaruvathil@gmail.com>
2023-01-14 16:35:36 +0530

    Added working menus (only relation editing left)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use nannou::prelude::*;
+use nannou_egui::{self, egui, Egui};
 
 fn main() {
     nannou::app(model).update(update).run();
@@ -8,35 +9,160 @@ struct Model {
     _window: window::Id,
     settings: Settings,
     atoms: Vec<Atom>,
+    egui: Egui,
 }
+fn init(app: &App, _window: window::Id) -> Model {
+    let win = app.window(_window).unwrap();
+    let egui = Egui::from_window(&win);
 
-fn model(app: &App) -> Model {
-    let _window = app.new_window().view(view).build().unwrap();
+    let (r_min, r_max) = (0.2, 50.0);
+    let friction = 0.2;
 
-    let (r_min, r_max) = (0.3, 100.0);
-    let friction = 0.5;
-    const num: usize = 100;
+    const num: usize = 1200;
+    const num_t: usize = 2;
+    let mut rel = Relation::new(num_t);
+    rel.set(0.03, 0 as usize, 0 as usize);
+    rel.set(0.015, 1 as usize, 0 as usize);
+    rel.set(-0.02, 0 as usize, 1 as usize);
+    rel.set(-0.02, 1 as usize, 1 as usize);
+
     let settings = Settings {
         r_min,
         r_max,
         friction,
         num,
+        num_t,
+        rel,
+        pn: num,
+        pnt: num_t,
     };
     let mut atoms: Vec<Atom> = vec![Atom::default(); num];
     let (bx, by) = app.window_rect().w_h();
-    for mut i in 0..atoms.len() {
+    for i in 0..atoms.len() {
         atoms[i].pos.x = random_range(-bx / 2.0, bx / 2.0);
         atoms[i].pos.y = random_range(-by / 2.0, by / 2.0);
+        atoms[i].t = (i % num_t) as usize;
     }
-
     Model {
         _window,
         settings,
         atoms,
+        egui,
     }
 }
+fn restart(app: &App, _window: window::Id, n: usize, n_t: usize) -> Model {
+    let m = init(&app, _window);
+    let num: usize = n;
+    let num_t: usize = n_t;
 
-fn update(_app: &App, model: &mut Model, update: Update) {}
+    let rel = Relation::new(num_t);
+
+    let settings = Settings {
+        r_min: m.settings.r_min,
+        r_max: m.settings.r_max,
+        friction: m.settings.friction,
+        num,
+        num_t,
+        rel,
+        pn: num,
+        pnt: num_t,
+    };
+    let mut atoms: Vec<Atom> = vec![Atom::default(); n];
+    let (bx, by) = app.window_rect().w_h();
+    for i in 0..atoms.len() {
+        atoms[i].pos.x = random_range(-bx / 2.0, bx / 2.0);
+        atoms[i].pos.y = random_range(-by / 2.0, by / 2.0);
+        atoms[i].t = (i % num_t) as usize;
+    }
+    let e = Egui::from_window(&(app.window(_window).unwrap()));
+    Model {
+        _window,
+        settings,
+        atoms,
+        egui: e,
+    }
+}
+fn model(app: &App) -> Model {
+    let _window = app
+        .new_window()
+        .title("Particle life")
+        .size(640, 320)
+        .raw_event(raw_events)
+        .event(events)
+        .view(view)
+        .build()
+        .unwrap();
+
+    init(&app, _window)
+}
+
+fn update(_app: &App, model: &mut Model, update: Update) {
+    //-------------------EGUI-------------------
+    let mut egui = &mut model.egui;
+    //let set = &mut model.settings;
+    egui.set_elapsed_time(update.since_start);
+
+    let c = egui.begin_frame();
+
+    egui::Window::new("Settings for particle life: ").show(&c, |ui| {
+        ui.label("Friction: ");
+        ui.add(egui::Slider::new(&mut model.settings.friction, 0.01..=0.75));
+
+        ui.label("Minumum distance:");
+        ui.add(egui::Slider::new(&mut model.settings.r_min, 0.01..=0.9));
+
+        ui.label("Maximum distance:");
+        ui.add(egui::Slider::new(&mut model.settings.r_max, 5.0..=200.0));
+
+        ui.label("Species:");
+        ui.add(egui::Slider::new(
+            &mut model.settings.pnt,
+            1 as usize..=5 as usize,
+        ));
+
+        ui.label("Number of particles:");
+        ui.add(egui::Slider::new(
+            &mut model.settings.pn,
+            500 as usize..=5000 as usize,
+        ));
+
+        //
+
+        //drop(c);
+    });
+    //------------------------------------------
+    if model.settings.pn != model.settings.num || model.settings.pnt != model.settings.num_t {
+        let mut dmod = restart(_app, model._window, model.settings.pn, model.settings.pnt);
+        model._window = dmod._window;
+        model.atoms = dmod.atoms;
+        //egui = &mut dmod.egui;
+        model.settings = dmod.settings;
+    }
+
+    for i in 0..model.atoms.len() {
+        let mut f = Vec2::ZERO;
+        for j in 0..model.atoms.len() {
+            if i == j {
+                continue;
+            }
+            f = f + model.atoms[i].get_force(&model.atoms[j], &model.settings);
+        }
+        //println!("{}", f);
+        model.atoms[i].apply_forces(f, &model.settings);
+        model.atoms[i].update();
+    }
+}
+fn events(_app: &App, model: &mut Model, event: WindowEvent) {
+    match event {
+        KeyPressed(_k) => {
+            //
+        }
+        _ => {}
+    }
+}
+fn raw_events(_app: &App, model: &mut Model, event: &nannou::winit::event::WindowEvent) {
+    model.egui.handle_raw_event(event);
+}
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
@@ -46,12 +172,17 @@ fn view(app: &App, model: &Model, frame: Frame) {
         i.draw(&draw);
     }
     draw.to_frame(app, &frame).unwrap();
+    model.egui.draw_to_frame(&frame).unwrap();
 }
 struct Settings {
     r_min: f32,
     r_max: f32,
     friction: f32,
     num: usize,
+    num_t: usize,
+    rel: Relation,
+    pn: usize,
+    pnt: usize,
 }
 #[derive(Copy, Clone, Debug)]
 struct Atom {
@@ -70,7 +201,9 @@ impl Atom {
             t: 0 as usize,
         }
     }
-    fn get_force(&self, p: &Atom, s: &Settings, rel: &Relation) -> Vec2 {
+    fn get_force(&self, p: &Atom, s: &Settings) -> Vec2 {
+        let rel = &s.rel;
+
         let delta = p.pos - self.pos;
         let mut d = delta.length();
         if d > s.r_max {
@@ -124,6 +257,10 @@ fn get_col(t: usize) -> Rgb<u8> {
         0 => SALMON,
         1 => SEAGREEN,
         2 => PLUM,
+        3 => INDIANRED,
+        4 => KHAKI,
+        5 => LIGHTCORAL,
+        6 => LIGHTPINK,
         _ => SEASHELL,
     }
 }
